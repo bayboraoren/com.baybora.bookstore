d190078182a5592836e2cd948b13907a69918320
baybora <baybora.oren@gmail.com>
2015-04-05 21:07:30 +0300

    configuration

---
+++ com.baybora.bookstore/com.baybora.bookstore.service/src/main/java/com/baybora/bookstore/service/config/ServiceConfiguration.java
@@ -0,0 +1,15 @@
+package com.baybora.bookstore.service.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+import com.baybora.bookstore.repository.config.RepositoryConfiguration;
+import com.baybora.bookstore.repository.config.TestDataContextConfiguration;
+
+@Configuration
+@ComponentScan(basePackages={"com.baybora.bookstore.service"})
+@Import(value={RepositoryConfiguration.class,TestDataContextConfiguration.class})
+public class ServiceConfiguration {
+
+}
